7ca70cde62c853fdcee9515089f0fc70933f4392
nick335 <102188095+nick335@users.noreply.github.com>
2022-03-28 21:16:25 +0100

    Add files via upload

--- chromeExtension.js
+++ chromeExtension.js
@@ -2,7 +2,7 @@ let myLeads=[];
 const inputEl=document.getElementById('input-el');
 const inputBtn=document.getElementById('input-btn');
 const ulEl=document.getElementById('ul-el');
-const leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"));
+let leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"));
 const deleteBtn=document.getElementById('delete-btn');
 const tabBtn=document.getElementById('tab-btn');
 //console.log(leadsFromLocalStorage);
@@ -41,12 +41,47 @@ deleteBtn.addEventListener('click', function() {
 })
 inputBtn.addEventListener("click", function () {
 	// body...
-	myLeads.push(inputEl.value)
-	inputEl.value="";
-	localStorage.setItem("myLeads", JSON.stringify(myLeads));
-	render(myLeads)
-} )
+	var leadsFromLocalStorage=	JSON.parse(localStorage.getItem("myLeads"));
+	if (inputEl.value === "") {
+		document.getElementById('alert').innerHTML=`<b>please Input leads</b>`;
+		setTimeout(function () {
+			// body...
+			document.getElementById('alert').innerHTML='';
+		}, 1000);
+	}else if (leadsFromLocalStorage === null){
+			console.log(leadsFromLocalStorage)
+			myLeads.push(inputEl.value)
+			inputEl.value="";
+			localStorage.setItem("myLeads", JSON.stringify(myLeads));
+			render(myLeads)
+	}else{
+ 			console.log(leadsFromLocalStorage)	
+			let check = checkDoubleLeads(leadsFromLocalStorage);
+			console.log(check)
+			if (check === true) {
+				document.getElementById('alert').innerHTML=`<b>lead already saved</b>`;
+				setTimeout(function () {
+				// body...
+				document.getElementById('alert').innerHTML='';
+				}, 1000);
+			 }else{
+				myLeads.push(inputEl.value)
+				inputEl.value="";
+				localStorage.setItem("myLeads", JSON.stringify(myLeads));
+				render(myLeads)
+			 }
+		}
+})
 
 //const li=document.createElement('li')
 	//li.textContent=myLeads[i]
 	//ulEl.append(li);
+function checkDoubleLeads(leadsArray) {
+	// body...
+	console.log(inputEl.value)
+	if (leadsArray.includes(inputEl.value)) {
+		return true;
+	}else{
+		return false;
+	}
+}
